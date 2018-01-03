322c3fd6e9587f2e6bf257dfc7d9f88c15b6372b
d3vk <31173267+d3vk@users.noreply.github.com>
2018-01-03 12:16:52 +0700

    Ganti apple jadi coin

--- manukan.cpp
+++ manukan.cpp
@@ -186,6 +186,7 @@ int al_game_container(Game Container) {
     Branch BoostItems;
     BoostItems = as_element_initialize<Branch>(WIDTH + 10, 0, 32, 32, 3, 0, -1, 0, "coin.png", "@_scoremux", 0);
 
+    
     ALLEGRO_FONT *font18 = al_load_font(FONT_CALIBRI, 18, 0);
 
     // audio start
